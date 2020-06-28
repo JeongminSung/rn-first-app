bb921eafcf69672274fc8c0325b4b2f27eeac035
성정민 <seongjeongmin@seongjeongmin-ui-MacBook-Pro.local>
2020-06-28 18:46:40 +0900

    git first commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('지금 중만의 상태는?');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
       <StatusBar style="auto" />
+      <Button title="눌러보세요" onPress={() => setOutputText('배가 몹시 고픈 상태')} />
     </View>
   );
 }
